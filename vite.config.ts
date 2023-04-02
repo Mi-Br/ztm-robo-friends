e40ab92ff627fcbeb2d0ed1da60721cb91a659f7
Michail <michailbredichin@gmail.com>
2023-04-02 07:50:37 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/ztm-robo-friends/"
+  base: "/ztm-robo-friends/",
+  plugins: [react()]
 })
